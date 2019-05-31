f971a0bc497c25be27714b95f92943d1dd84b9b0
Lautaro Umpierrez <lauta.guillermo@gmail.com>
2019-05-31 20:49:31 -0300

    Create CookieManager.js

---
+++ CookieManager.js
@@ -0,0 +1,46 @@
+
+// Cookie Manager is writted by Lautaro Umpierrez && it do all actions manipulating the localStorage.
+class CookieManager {
+  removeCookie(cookieName){
+    return new Promise((resolve, reject)=>{
+      if(window.localStorage.getItem(cookieName)){
+        window.localStorage.removeItem(cookieName);
+        resolve();
+      }else{
+        reject(`The cookie with the name {${cookieName}} dont exists or anything`);
+      }
+    });
+  }
+  getCookie(cookieName){
+    return new Promise((resolve,reject)=>{
+      let cookie = JSON.parse(window.localStorage.getItem(cookieName));
+      if(window.localStorage.getItem(cookieName !== null)){
+        if(cookie.expirationTime <= Date.now()){
+          window.localStorage.removeItem(cookieName);
+          reject('The cookie are expired');
+        }else{
+          resolve(cookie.data);
+        }
+      }else{
+        reject('The Cookie Name Dont working');
+      }
+    });
+  }
+  setCookie(cookieName, cookieData){
+    return new Promise((resolve,reject)=>{
+      if(cookieName !== null && cookieData !== null)
+      {
+        let cookie = {
+          expirationTime: Date.now() + 1000*60*60*24*30,
+          data: cookieData
+        };
+        window.localStorage.setItem(cookieName, JSON.stringify(cookieData));
+        resolve({created: true});
+      }else{
+        reject('{cookieName} && {cookieData} is necessary for use the setCookie method setCookie("cookieName", {//cookie data})')
+      }
+    });
+  }
+}
+
+export default CookieManager;
